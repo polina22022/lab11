02982a196c123621e67457b875106d03ea37c9f3
Polina Medvedeva <polinamed.22.02@mail.ru>
2025-11-18 17:25:20 +0300

    Update lab11.c

--- lab11.c
+++ lab11.c
@@ -1,52 +1,16 @@
-#include <stdio.h>
-#include <locale.h>
-int main() {
-    setlocale(LC_ALL, "RUS");
-    int n;
-    printf("Ââåäèòå êîëè÷åñòâî ýëåìåíòîâ ìàññèâà: ");
-    scanf("%d", &n);
-
-    int array[n];
-
-    printf("Ââåäèòå %d öåëûõ ÷èñåë:\n", n);
-    for (int i = 0; i < n; i++) {
-        scanf("%d", &array[i]);
-    }
-
-    int max = array[0];
-    int max_index = 0;
-    for (int i = 1; i < n; i++) {
-        if (array[i] > max) {
-            max = array[i];
-            max_index = i;
-        }
-    }
-    
-    int count = 0;
-    for (int i = 0; i < max_index; i++) {
-        if (array[i] > 0) {
-            count++;
-        }
-    }
-
-    printf("Êîëè÷åñòâî ïîëîæèòåëüíûõ ýëåìåíòîâ äî ìàêñèìàëüíîãî: %d\n", count);
-
-    return 0;
-}
-после лабы
 #define _CRT_SECURE_NO_DEPRECATE
 #include <stdio.h>
 #include <locale.h>
 #define SIZE 1000
 int main() {
-    setlocale(LC_ALL, "RUS");
     int n;
-    printf("Введите  положительных и отрицательных чисел: ");
+    setlocale(LC_ALL, "RUS");
+    printf("Введите размер массива: ");
     scanf("%d", &n);
 
     int array[SIZE];
 
-    printf("������� %d ����� �����:\n", n);
+    printf("Введите %d элементов массива:\n", n);
     for (int i = 0; i < n; i++) {
         scanf("%d", &array[i]);
     }
@@ -59,7 +23,6 @@ int main() {
             max_index = i;
         }
     }
-
     int count = 0;
     for (int i = 0; i < max_index; i++) {
         if (array[i] > 0) {
@@ -67,7 +30,7 @@ int main() {
         }
     }
 
-    printf("���������� ������������� ��������� �� �������������: %d\n", count);
+    printf("Количество положительных элементов, расположенных до максимального: %d\n", count);
 
     return 0;
 }
